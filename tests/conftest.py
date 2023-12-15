aaf6f2c322329d7ec6e9a6d0d8119f5ea0da3740
uaipedro <pedromambelli@gmail.com>
2023-12-15 20:42:13 -0300

    Add database configuration and models

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from fast_zero.app import create_app
+from fast_zero.models import Base
 
 
 @pytest.fixture
@@ -11,3 +14,12 @@ def app():
 @pytest.fixture
 def client(app):
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    yield Session()
+    Base.metadata.drop_all(engine)
---
+++ tests/test_db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import select
+from fast_zero.models import User
+
+
+def test_create_user(session):
+    new_user = User(username='alice', password='secret', email='teste@test')
+    session.add(new_user)
+    session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'alice'))
+
+    assert user.username == 'alice'
+    assert user.password == 'secret'
+    assert user.email == 'teste@test'

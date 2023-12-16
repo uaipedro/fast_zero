180d240e1436399885d15f89b9549ab8059cd6ed
uaipedro <pedromambelli@gmail.com>
2023-12-16 17:34:53 -0300

    Add test cases for token expiration and incorrect credentials

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,3 +1,6 @@
+from freezegun import freeze_time
+
+
 def test_get_token(client, user):
     response = client.post(
         '/auth/token',
@@ -8,3 +11,43 @@ def test_get_token(client, user):
     assert response.status_code == 200
     assert 'access_token' in token
     assert 'token_type' in token
+
+
+def test_token_expired_after_time(client, user):
+    with freeze_time('2024-01-01 12:00:00'):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password},
+        )
+        assert response.status_code == 200
+        token = response.json()['access_token']
+
+    with freeze_time('2024-01-01 12:31:00'):
+        response = client.put(
+            f'users/{user.id}',
+            headers={'Authorization': f'Bearer {token}'},
+            json={
+                'email': 'wrong@email.com',
+                'password': 'wrongpassword',
+            },
+        )
+        assert response.status_code == 401
+        assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_token_inexistent_user(client):
+    response = client.post(
+        '/auth/token',
+        data={'username': 'no_username@domain.com', 'password': 'no_password'},
+    )
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+def test_token_wrong_password(client, user):
+    response = client.post(
+        '/auth/token',
+        data={'username': user.email, 'password': 'wrongpassword'},
+    )
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Incorrect email or password'}

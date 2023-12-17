d1ccdcb734fc2bf114201126b2d112c845dcd0f3
uaipedro <pedromambelli@gmail.com>
2023-12-16 21:32:20 -0300

    Add token refresh functionality

--- fast_zero/routes/auth.py
+++ fast_zero/routes/auth.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
@@ -8,16 +10,21 @@ from fast_zero.models import User
 from fast_zero.schemas import Token
 from fast_zero.security import (
     create_access_token,
+    get_current_user,
     verify_password,
 )
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
+Session = Annotated[Session, Depends(get_session)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
+RequestForm = Annotated[OAuth2PasswordRequestForm, Depends()]
+
 
 @router.post('/token', response_model=Token)
 def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: Session = Depends(get_session),
+    session: Session,
+    form_data: RequestForm,
 ):
     user = session.scalar(select(User).where(User.email == form_data.username))
 
@@ -34,3 +41,12 @@ def login_for_access_token(
     access_token = create_access_token(data={'sub': user.email})
 
     return {'access_token': access_token, 'token_type': 'bearer'}
+
+
+@router.post('/refresh_token', response_model=Token)
+def refresh_access_token(
+    user: CurrentUser,
+):
+    new_access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': new_access_token, 'token_type': 'bearer'}
